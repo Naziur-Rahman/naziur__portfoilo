156ca22da3c1962d972f79e1f50b05da736097b2
Naziur-Rahman <naziurr85@gmail.com>
2024-03-17 16:40:13 +0600

    qualification loadiiing

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -6,6 +6,29 @@ function Qualification() {
         <h2 className="section__title">Qualification</h2>
         <span className="section__subtitle">My personal journey</span>
 
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className="qualification__button qualification__active button__flex">
+                    <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                </div>
+                <div className="qualification__button button__flex">
+                    <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                </div>
+            </div>
+            <div className="qualification__sections">
+                <div className="qualification__content">
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title"></h3>
+                            <span className="qualification__subtitle"></span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
