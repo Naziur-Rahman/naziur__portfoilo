8eecb35807d10661470f72113467b0d47041b9ff
Naziur-Rahman <naziurr85@gmail.com>
2024-03-18 00:23:36 +0600

    testimonial section loading

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./testimonials.css";
+import { data } from 'autoprefixer';
+function Testimonials() {
+  return (
+    <section className="testimonial container section">
+        <h2 className="section__title">My clients say</h2>
+        <span className="section__subtitle">Testimonial</span>
+
+        <div className="testimonial__container">
+
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
