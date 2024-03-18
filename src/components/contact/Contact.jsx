1d372ec524a36ae923becc41a7c00b57e7399de1
Naziur-Rahman <naziurr85@gmail.com>
2024-03-18 13:19:27 +0600

    contact form is loading

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,19 @@ function Contact() {
                 <div className="contact__info">
                     <div className="contact__card">
                         <i className="bx bx contact__card__icon"></i>
-                        <h3 className="contact__card__title"></h3>
+                        <h3 className="contact__card__title">Email</h3>
+                        <span className="contact__card__data">naziurr85@gmail.com</span>
+                        <a href="" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button__icon"></i></a>
+                    </div>
+                    <div className="contact__card">
+                        <i className="bx bx contact__card__icon"></i>
+                        <h3 className="contact__card__title">Whatsapp</h3>
+                        <span className="contact__card__data">+8801871950601</span>
+                        <a href="" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button__icon"></i></a>
+                    </div>
+                    <div className="contact__card">
+                        <i className="bx bx contact__card__icon"></i>
+                        <h3 className="contact__card__title">Messenger</h3>
                         <span className="contact__card__data"></span>
                         <a href="" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button__icon"></i></a>
                     </div>
